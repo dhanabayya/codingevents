ab3c5ee8d7e49217a1849dc5c3f1b0af979be0b6
Dhana bayya <krishnabayya@Dhanas-mac-Air.lan>
2024-12-05 08:49:11 -0600

    show events

---
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -0,0 +1,24 @@
+package org.launchcode.codingevents.controllers;
+
+import ch.qos.logback.core.model.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("events")
+public class EventController {
+
+    public String showEvents(@RequestParam String name, Model model) {
+        List<String> events = new ArrayList<>();
+        events.add("Code With Pride");
+        events.add("Strange Loop");
+        events.add("Apple WWDC");
+        events.add("SpringOne Platform");
+        model.addText("events");
+        return "events/index";
+    }
+}
