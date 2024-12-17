ebdbc02da8018a374fddc1efe886c1d5665ad410
Dhana bayya <krishnabayya@Dhanas-mac-Air.lan>
2024-12-17 10:11:13 -0600

    error msg applied

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,9 +1,11 @@
 package org.launchcode.codingevents.controllers;
 
+import jakarta.validation.Valid;
 import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -26,7 +28,12 @@ public class EventController {
 
     //lives at /events/create
     @PostMapping("create")
-    public String createEvent(@ModelAttribute Event newEvent) {
+    public String createEvent(@ModelAttribute @Valid Event newEvent, Errors errors,Model model) {
+        if(errors.hasErrors()){
+            model.addAttribute("events", EventData.getAll());
+            model.addAttribute("errorMsg","Bad data!");
+            return "events/create";
+        }
         EventData.add(newEvent);
         return ("redirect:/events");
     }
